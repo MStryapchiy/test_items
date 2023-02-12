117f648a9ae10ad1811686be3a6b1fd05434b302
MStryapchiy <MustStr@gmail.com>
2023-02-12 20:54:35 +0400

    change method name

--- test_items.py
+++ test_items.py
@@ -4,7 +4,7 @@ import time
 link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 
 class TestItems():
-    def test_items(self, browser):
+    def test_search_for_add_to_bascket_button(self, browser):
         browser.get(link)
         browser.implicitly_wait(5)
         assert browser.find_element(By.CSS_SELECTOR, ".btn-add-to-basket")
